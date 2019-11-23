d03bc654e8c2a17ff34298b8ec63961a78057e31
Xanthin <x.paris@rubika-edu.com>
2019-11-23 13:49:46 +0100

    Début Procédural (Back-Up)

---
+++ Projet-RogueLike-Egopt/Assets/XP_LevelGeneration/XP_RoomSpawner.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XP_RoomSpawner : MonoBehaviour
+{
+    public int openingDirection;
+    // 1--> Need bottom door.
+    // 2--> Need top door.
+    // 3--> Need left door.
+    // 4--> need right door.
+}
