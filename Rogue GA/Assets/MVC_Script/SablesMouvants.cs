c87124ffd35d83f2cf157ed41e9705f35e57115e
mathieu-hub <m.vancappellen@rubika-edu.com>
2019-11-23 23:42:43 +0100

    coucou j'ai tous cassé

---
+++ Rogue GA/Assets/MVC_Script/SablesMouvants.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SablesMouvants : MonoBehaviour
+{
+    public GameObject player;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        player.GetComponent<PlayerMovement>().moveSpeed = 0.5f;
+        Debug.Log("le jouer est ralentit");
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        player.GetComponent<PlayerMovement>().moveSpeed = 1.5f;
+        Debug.Log("le jouer n'est plus ralentit");
+    }
+}
