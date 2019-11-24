11a22de7464ac4cd8eed1b45399f6f32a6c00e9e
mathieu-hub <m.vancappellen@rubika-edu.com>
2019-11-24 23:45:11 +0100

    Compteurs de Golds et Scoring

---
+++ Projet-RogueLike-Egopt/Assets/MVC/MVC_Script/RecuperateCoin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecuperateCoin : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            GoldCounterScript.goldsCounter += 10;
+            Destroy(gameObject);
+        }
+    }
+
+
+
+}
