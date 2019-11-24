fc48cbd662fc6ca6bd91e9792294fa0c2abd7808
Xanthin <x.paris@rubika-edu.com>
2019-11-24 21:48:55 +0100

    4/7 Salles

---
+++ Projet-RogueLike-Egopt/Assets/Assets/MVC_Script/SablesMouvants.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SablesMouvants : MonoBehaviour
+{
+    public GameObject player;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        player.GetComponent<PlayerMovement>().speed = 0.5f;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        player.GetComponent<PlayerMovement>().speed = 1.5f;
+    }
+}
---
+++ Projet-RogueLike-Egopt/Assets/Assets/MVC_Script/TrappeTraps.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrappeTraps : MonoBehaviour
+{
+   /* private SpriteRenderer spr;
+    public Sprite openTrappe;
+    public Sprite closeTrappe;
+    bool isOpen = false;
+    bool isActivated = false;
+    public GameObject respawnPoint;
+
+    private void Start()
+    {
+        spr = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isActivated == false)
+        {
+            StartCoroutine(LoadOpenTrappe());
+        }
+
+        IEnumerator LoadOpenTrappe()
+        {
+            //début animation
+            isActivated = true;
+            yield return new WaitForSeconds(1);    //à remplacer par un trigger de fin d'animation
+            print("la trappe est ouverte");
+            isOpen = true;
+            spr.sprite = openTrappe;
+
+            //début d'animation
+            yield return new WaitForSeconds(5);
+            print("la trappe est fermé");
+            isOpen = false;
+            spr.sprite = closeTrappe;
+            isActivated = false;
+        }
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (isOpen == true && collision.gameObject.tag == "Player") // les ennemis aussi sont impactés !!!
+        {
+            StartCoroutine(Fallen());
+
+            IEnumerator Fallen()
+            {
+                print("Fallen Start Execution");
+                collision.GetComponent<PlayerMovement>().canMove = false;
+                yield return new WaitForSeconds(1);
+                print("Fallen is finish");
+                collision.transform.position = respawnPoint.transform.position;
+                print("Player is Respawn");
+                collision.GetComponent<PlayerMovement>().canMove = true;
+                // Ajouter la perte de vie
+            }
+
+        }
+    }*/
+}
