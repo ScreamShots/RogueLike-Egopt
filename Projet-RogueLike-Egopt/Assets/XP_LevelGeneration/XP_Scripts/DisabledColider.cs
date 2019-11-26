202f7a141a751c435b57ec25e987f800d9b538e5
Xanthin <x.paris@rubika-edu.com>
2019-11-26 11:04:14 +0100

    Clean Box Collider MiddleCenter

---
+++ Projet-RogueLike-Egopt/Assets/XP_LevelGeneration/XP_Scripts/DisabledColider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisabledColider : MonoBehaviour
+{
+    private int numberOfSpawnedRooms;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        numberOfSpawnedRooms = GameObject.FindGameObjectsWithTag("Rooms").Length;
+
+        if (numberOfSpawnedRooms >= 15)
+        {
+            GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+}
